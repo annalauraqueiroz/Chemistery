6a6247fa892cf6487dd410f49dd1654568fafdb2
Anna <annalauraqueiroz@hotmail.com>
2023-05-08 19:01:24 -0300

    first steps with reactJs

---
+++ reactapp/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component, useState } from 'react';
+import Input from './components/Input.js'
+export default class App extends Component {
+    static displayName = App.name;
+
+
+    render() {
+        
+
+        return (
+            <div>
+               
+                <Input/>
+
+            </div>
+        );
+    }
+
+    
+}
---
+++ reactapp/src/components/Input.js
@@ -0,0 +1,48 @@
+import React, { useState, useRef } from 'react';
+import useFetch from 'react-fetch-hook';
+
+
+export default function Form() {
+  const inputRef = useRef(null);
+  const atomicNInput = useRef(null);
+
+  const [equation, setEquation] = useState('');
+
+ const data = useFetch('https://localhost:7180/api/PeriodicTable/nAtomico/4');
+
+  const handleClick = () => {
+    getData(inputRef.current.value);
+
+  }
+  async function getData(name) {
+    const encoded = encodeURI('https://localhost:7180/api/PeriodicTable/balanceamento/' + name)
+
+    await fetch(encoded, {
+      method: 'get',
+    })
+      .then(res => { return res.text() })
+      .then(
+        (result) => {
+          setEquation(result);
+        }
+      )
+      .catch((err) => console.log(err))
+  }
+  return (
+    <>
+      <label>
+        Equation
+        <input
+          type="text"
+          ref={inputRef}
+          name="equation"
+        />
+        <button onClick={handleClick}>
+          Balance
+        </button>
+        <h2>Equacao {equation}</h2>
+      </label>
+
+    </>
+  );
+}
